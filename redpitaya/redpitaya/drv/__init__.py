2b76e99125b6b3889f810971f42b60bf2d003003
Iztok Jeras <iztok.jeras@redpitaya.com>
2017-04-04 13:13:54 +0200

    added separate LED example

--- redpitaya/redpitaya/drv/__init__.py
+++ redpitaya/redpitaya/drv/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['mercury', 'overlay', 'uio', 'hwid', 'pdm', 'clb', 'gen', 'osc']
+__all__ = ['mercury', 'overlay', 'uio', 'hwid', 'pdm', 'clb', 'gen', 'osc', 'led']
---
+++ redpitaya/redpitaya/drv/led.py
@@ -0,0 +1,4 @@
+from periphery import LED
+
+class led (LED):
+    pass
