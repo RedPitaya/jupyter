f3adb5ae75fb224fce5b52829740d3900b3015ea
Iztok Jeras <iztok.jeras@redpitaya.com>
2017-04-05 12:30:40 +0200

    analog input update

--- redpitaya/redpitaya/overlay/mercury.py
+++ redpitaya/redpitaya/overlay/mercury.py
@@ -9,12 +9,18 @@ from redpitaya.drv.clb     import clb
 from redpitaya.drv.gen     import gen
 from redpitaya.drv.osc     import osc
 
+import iio
+
 class mercury (overlay):
     def __init__ (self):
         super().__init__ (overlay = 'mercury')
 
+    def __del__ (self):
+        super().__del__ ()
+
     class led (LED):
         leds = range(8)
+
         def __init__ (self, index, brightness=None):
             if index not in self.leds:
                 raise ValueError("LED index should be one of: {}".format(self.leds))
@@ -24,6 +30,7 @@ class mercury (overlay):
     class gpio (GPIO):
         ports = {'p': 968, 'n': 976}
         gpios = range(8)
+
         def __init__ (self, port, pin, direction="preserve"):
             if port not in self.ports:
                 raise ValueError("GPIO port should be one of: {}".format(self.ports))
@@ -31,6 +38,23 @@ class mercury (overlay):
                 raise ValueError("GPIO pin should be one of: {}".format(self.gpios))
             else:
                 super().__init__ (pin = self.ports[port] + pin, direction = direction)
+
+    class analog_in ():
+        channels = {0:'vaux8', 1:'vaux0', 2:'vaux1', 3:'vaux9'}
+        ctx = iio.Context()
+        dev = ctx.devices[3]
+        # resistor divider
+        resdiv = 4.99 / (30.0 + 4.99)
+
+        def __init__ (self, channel):
+            if channel in range(4):
+                channel = self.channels[channel]
+            self.chn   = self.dev.find_channel(channel)
+            self.scale = self.chn.attrs['scale'].value
+
+        def read(self):
+            raw = self.chn.attrs['raw'].value
+            return (int(raw)*float(self.scale)/1000 / self.resdiv)
 
     class hwid (hwid):
         pass
