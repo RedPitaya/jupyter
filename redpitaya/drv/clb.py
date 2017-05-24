6deb9bf8cece7b9982a5d8e529857ed04bbf8277
Iztok Jeras <iztok.jeras@gmail.com>
2017-05-24 15:57:01 +0200

    calibration: using 'ctypes' for structures

--- redpitaya/drv/clb.py
+++ redpitaya/drv/clb.py
@@ -1,87 +1,55 @@
 import numpy as np
+from ctypes import *
 
 from .uio import uio
 
+# FPGA regset structure
+class _regset_channel_t (Structure):
+    _fields_ = [('cfg_mul', c_int32),  # multiplication
+                ('cfg_sum', c_int32)]  # summation
+class _regset_t (Structure):
+    _fields_ = [('dac', _regset_channel_t * 2),  # generator
+                ('adc', _regset_channel_t * 2)]  # oscilloscope
+
+# floating point structure
+class _clb_channel_t (Structure):
+    _fields_ = [('gain'  , c_float),  # multiplication
+                ('offset', c_float)]  # summation
+class _clb_range_t (Structure):
+    _fields_ = [('lo', _clb_channel_t),  #  1.0V range
+                ('hi', _clb_channel_t)]  # 20.0V range
+class _clb_t (Structure):
+    _fields_ = [('dac', _clb_channel_t * 2),  # generator
+                ('adc', _clb_range_t   * 2)]  # oscilloscope
+
+# EEPROM structure
+class _eeprom_t (Structure):
+    _fields_ = [('adc_hi_gain'  , c_uint32 * 2),
+                ('adc_lo_gain'  , c_uint32 * 2),
+                ('adc_lo_offset',  c_int32 * 2),
+                ('dac_gain'     , c_uint32 * 2),
+                ('dac_offset'   ,  c_int32 * 2),
+                ('magic'        , c_uint32    ),
+                ('adc_hi_offset',  c_int32 * 2)]
+
 class clb (uio):
-    channels_adc = range(2)
     channels_dac = range(2)
+    channels_adc = range(2)
 
     _MAGIC = 0xAABBCCDD
     _eeprom_device = "/sys/bus/i2c/devices/0-0050/eeprom"
     _eeprom_offset_user    = 0x0008
     _eeprom_offset_factory = 0x1c08
 
-    # FPGA regset structure
-    _regset_channel_dtype = np.dtype([
-        ('cfg_mul', 'int32'),  # multiplication
-        ('cfg_sum', 'int32')   # summation
-    ])
-    _regset_dtype = np.dtype([
-        ('dac', _regset_channel_dtype, 2),  # generator
-        ('adc', _regset_channel_dtype, 2),  # oscilloscope
-    ])
-
-    # floating point structure
-    _clb_channel_dtype = np.dtype([
-        ('gain'  , 'float32'),  # multiplication
-        ('offset', 'float32')   # summation
-    ])
-    _clb_range_dtype = np.dtype([
-        ('lo', _clb_channel_dtype),  #  1.0V range
-        ('hi', _clb_channel_dtype)   # 20.0V range
-    ])
-    _clb_dtype = np.dtype([
-        ('adc', _clb_range_dtype  , 2),  # oscilloscope
-        ('dac', _clb_channel_dtype, 2),  # generator
-    ])
-
-    # EEPROM structure
-    _eeprom_dtype = np.dtype([
-        ('adc_hi_gain'  , 'uint32', 2),
-        ('adc_lo_gain'  , 'uint32', 2),
-        ('adc_lo_offset',  'int32', 2),
-        ('dac_gain'     , 'uint32', 2),
-        ('dac_offset'   ,  'int32', 2),
-        ('magic'        , 'uint32'),
-        ('adc_hi_offset',  'int32', 2)
-    ])
-
     def __init__ (self, uio:str = '/dev/uio/clb'):
         super().__init__(uio)
-        regset_array = np.recarray(1, self._regset_dtype, buf=self.uio_mmaps[0])
-        self.regset = regset_array[0]
+        self.regset = _regset_t.from_buffer(self.uio_mmaps[0])
 
-        self.adc = [self.ADC(self.regset.adc[ch]) for ch in self.channels_adc]
         self.dac = [self.DAC(self.regset.dac[ch]) for ch in self.channels_dac]
+        self.adc = [self.ADC(self.regset.adc[ch]) for ch in self.channels_adc]
 
     def __del__ (self):
         super().__del__()
-
-    class ADC (object):
-        DW = 16
-        _DWr = 2**DW - 1
-        _DW1 = 2**(DW-2)
-
-        def __init__ (self, regset):
-            self.regset = regset
-
-        @property
-        def gain (self) -> float:
-            """ADC gain calibration."""
-            return (self.regset.cfg_mul / self._DW1)
-
-        @gain.setter
-        def gain (self, gain: float):
-            self.regset.cfg_mul = int(gain * self._DW1)
-
-        @property
-        def offset (self) -> float:
-            """ADC offset calibration."""
-            return (self.regset.cfg_sum / self._DWr)
-
-        @offset.setter
-        def offset (self, offset: float):
-            self.regset.cfg_mul = int(offset * self._DWr)
 
     class DAC (object):
         DW = 14
@@ -107,7 +75,33 @@ class clb (uio):
 
         @offset.setter
         def offset (self, offset: float):
-            self.regset.cfg_mul = int(offset * self._DWr)
+            self.regset.cfg_sum = int(offset * self._DWr)
+
+    class ADC (object):
+        DW = 16
+        _DWr = 2**DW - 1
+        _DW1 = 2**(DW-2)
+
+        def __init__ (self, regset):
+            self.regset = regset
+
+        @property
+        def gain (self) -> float:
+            """ADC gain calibration."""
+            return (float(self.regset.cfg_mul) / self._DW1)
+
+        @gain.setter
+        def gain (self, gain: float):
+            self.regset.cfg_mul = int(gain * self._DW1)
+
+        @property
+        def offset (self) -> float:
+            """ADC offset calibration."""
+            return (self.regset.cfg_sum / self._DWr)
+
+        @offset.setter
+        def offset (self, offset: float):
+            self.regset.cfg_sum = int(offset * self._DWr)
 
     def eeprom_read (self, eeprom_offset = _eeprom_offset_user):
         # open EEPROM device
@@ -124,7 +118,7 @@ class clb (uio):
 
         # read calibration data into a buffer
         try:
-            buffer = eeprom_file.read (self._eeprom_dtype.itemsize)
+            buffer = eeprom_file.read (sizeof(_eeprom_t))
         except IOError as e:
             raise IOError(e.errno, "Read {}: {}".format(uio, e.strerror))
 
@@ -135,9 +129,7 @@ class clb (uio):
             raise IOError(e.errno, "Close {}: {}".format(uio, e.strerror))
 
         # map buffer onto structure
-        eeprom_array = np.recarray(1, self._eeprom_dtype, buf=buffer)
-        eeprom_struct = eeprom_array[0]
-
+        eeprom_struct = _eeprom_t.from_buffer_copy(buffer)
         return (eeprom_struct)
 
     def FullScaleToVoltage(self, cnt: int) -> float:
@@ -152,8 +144,7 @@ class clb (uio):
     def eeprom_parse (self, eeprom_struct):
 
         # return structure
-        clb_array = np.recarray(1, self._clb_dtype)
-        clb_struct = clb_array[0]
+        clb_struct = _clb_t()
 
         # convert EEPROM values into local float values
         for ch in self.channels_adc:
@@ -179,7 +170,7 @@ class clb (uio):
 
         return (clb_struct)
 
-    def show_float (self, clb_struct):
+    def calib_show (self, clb_struct):
         for ch in self.channels_adc:
             print('adc[{}].lo.gain   = {}'.format(ch, clb_struct.adc[ch].lo.gain))
             print('adc[{}].hi.gain   = {}'.format(ch, clb_struct.adc[ch].hi.gain))
@@ -188,3 +179,17 @@ class clb (uio):
         for ch in self.channels_dac:
             print('dac[{}].gain      = {}'.format(ch, clb_struct.dac[ch].gain))
             print('dac[{}].offset    = {}'.format(ch, clb_struct.dac[ch].offset))
+
+    def calib_apply (self, clb_struct, adc_range = ['lo', 'lo']):
+        for ch in self.channels_adc:
+            if   (adc_range[ch] == 'lo'):
+                self.adc[ch].gain   = clb_struct.adc[ch].lo.gain
+                self.adc[ch].offset = clb_struct.adc[ch].lo.offset
+            elif (adc_range[ch] == 'hi'):
+                self.adc[ch].gain   = clb_struct.adc[ch].hi.gain
+                self.adc[ch].offset = clb_struct.adc[ch].hi.offset
+            else:
+                raise ValueError("ADC range can be one of ['lo', 'hi'].")
+        for ch in self.channels_dac:
+            self.dac[ch].gain   = clb_struct.dac[ch].gain
+            self.dac[ch].offset = clb_struct.dac[ch].offset
