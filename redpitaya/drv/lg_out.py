f89aaf56c9e6a0401667ff2be1a2f0f27e12aec6
Iztok Jeras <iztok.jeras@gmail.com>
2017-06-02 16:49:08 +0200

    LG: updated output control regset, added documentation

--- redpitaya/drv/lg_out.py
+++ redpitaya/drv/lg_out.py
@@ -2,6 +2,27 @@ from ctypes import *
 import numpy as np
 
 class lg_out (object):
+    """Logic generator output control.
+    
+    +--------------+--------------+-------------+----------------+-------------+-------------------------------+
+    | ``cfg_oe0``  | ``cfg_oe1``  | ``cfg_msk`` | ``cfg_val``    | **output**  |  **output**                   |
+    | out enable 0 | out enable 1 |  out mask   | value/polarity | **enable**  | **equation**                  |
+    +==============+==============+=============+================+=============+===============================+
+    | 0            | 0            | x           | x              | Hi-Z        | ``z``                         |
+    +--------------+--------------+-------------+----------------+-------------+-------------------------------+
+    | 0            | 1            | 0           |                | open drain  | ``          cfg_val ? 1 : z`` |
+    +--------------+--------------+-------------+----------------+-------------+-------------------------------+
+    | 0            | 1            | 1           |                | open drain  | ``asg_val ^ cfg_val ? 1 : z`` |
+    +--------------+--------------+-------------+----------------+-------------+-------------------------------+
+    | 1            | 0            | 0           |                | open source | ``          cfg_val ? z : 0`` | 
+    +--------------+--------------+-------------+----------------+-------------+-------------------------------+
+    | 1            | 0            | 1           |                | open source | ``asg_val ^ cfg_val ? z : 0`` |
+    +--------------+--------------+-------------+----------------+-------------+-------------------------------+
+    | 1            | 1            | 0           |                | push-pull   | ``          cfg_val``         |
+    +--------------+--------------+-------------+----------------+-------------+-------------------------------+
+    | 1            | 1            | 1           |                | push-pull   | ``asg_val ^ cfg_val``         |
+    +--------------+--------------+-------------+----------------+-------------+-------------------------------+
+    """
     class _regset_t (Structure):
         _fields_ = [('cfg_oen', c_uint32),  # output enable
                     ('cfg_msk', c_uint32),  # bit mask
@@ -11,20 +32,20 @@ class lg_out (object):
     def show_regset (self):
         """Print FPGA module register set for debugging purposes."""
         print (
-            "cfg_oen = 0x{reg:08x} = {reg:10d}  # output enable     \n".format(reg=self.regset.out.cfg_oen)+
-            "cfg_msk = 0x{reg:08x} = {reg:10d}  # bit mask          \n".format(reg=self.regset.out.cfg_msk)+
-            "cfg_val = 0x{reg:08x} = {reg:10d}  # bit value         \n".format(reg=self.regset.out.cfg_val)+
-            "cfg_pol = 0x{reg:08x} = {reg:10d}  # polarity inversion\n".format(reg=self.regset.out.cfg_pol)
+            "cfg_oe0 = 0x{reg:08x} = {reg:10d}  # output enable 0\n".format(reg=self.regset.out.cfg_oe0)+
+            "cfg_oe1 = 0x{reg:08x} = {reg:10d}  # output enable 1\n".format(reg=self.regset.out.cfg_oe1)+
+            "cfg_msk = 0x{reg:08x} = {reg:10d}  # output mask    \n".format(reg=self.regset.out.cfg_msk)+
+            "cfg_val = 0x{reg:08x} = {reg:10d}  # value/polarity \n".format(reg=self.regset.out.cfg_val)
         )
 
     @property
-    def enable (self) -> int:
-        """Output enable."""
-        return (self.regset.out.cfg_oen)
+    def enable (self) -> tuple:
+        """Output enable [oe0, oe1]."""
+        return ([self.regset.out.cfg_oe0, self.regset.out.cfg_oe1])
 
     @enable.setter
-    def enable (self, value: int):
-        self.regset.out.cfg_oen = value
+    def enable (self, value: tuple):
+        [self.regset.out.cfg_oe0, self.regset.out.cfg_oe1] = value
 
     @property
     def mask (self) -> int:
@@ -43,12 +64,3 @@ class lg_out (object):
     @value.setter
     def value (self, value: int):
         self.regset.out.cfg_val = value
-
-    @property
-    def polarity (self) -> int:
-        """Output polarity inversion."""
-        return (self.regset.out.cfg_pol)
-
-    @polarity.setter
-    def polarity (self, value: int):
-        self.regset.out.cfg_pol = value
