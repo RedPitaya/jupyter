d9e077e86a15ea6125ce414dacd70a22b28d6195
Iztok Jeras <iztok.jeras@gmail.com>
2017-06-29 15:11:40 +0200

    untested fixed point class

---
+++ redpitaya/drv/fixp.py
@@ -0,0 +1,42 @@
+class fixp (object):
+    """Fixed point basic class."""
+    __init__(self, s: bool, m: int, f: int):
+        self.s = s
+        self.m = m
+        self.f = f
+
+    @property
+    def num(self) -> int:
+        """Number of possible values."""
+        return(         (1<<(v.s+v.m+v.f))        )
+
+    @property
+    def max(self) -> int:
+        """Maximum value (positive)."""
+        return(       (+(1<<(    v.m+v.f))-1)     )
+
+    @property
+    def min(self) -> int:
+        """Minimum value (nagetive or zero for unsigned)."""
+        return((v.s ? (-(1<<(    v.m+v.f))  ) : 0))
+
+    @property
+    def unit(self) -> int:
+        """Unit (1) value."""
+        return(         (1<<(        v.f))        )
+
+    @property
+    def bits(self) -> int:
+        """Number of used bits."""
+        return(             (v.s+v.m+v.f)         )
+
+    def float2fixp(self, value: float) -> int:
+        """Conversion from ``float`` fixed point."""
+        if ((not self.s) and (value < 0)):
+        else:
+            return(int(value*self.unit))
+
+    def fixp2float(self, value: int) -> float:
+        """Conversion from ``float`` fixed point."""
+        return(float(value)/float(self.unit))
+        
